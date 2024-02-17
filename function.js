9ed56b82395971b820eee678d266f76e5698fccc
Sikander700 <158577768+Sikander700@users.noreply.github.com>
2024-02-17 19:12:35 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+function add(a,b){
+
+    c=a+b;
+    console.log(c);
+}
+
+add(873, 34);
+
+console.log(c);
---
+++ keywords.js
@@ -0,0 +1,15 @@
+if(68 % 2 === 0){
+    cconsole.log('even');
+
+    num1=3674;
+
+   let num2=3874;
+   const pi=3.1416;
+   num2=pi + 3456;
+
+
+}else{
+    console.log('odd');
+}
+console.log(num1);
+console.log(num2);
